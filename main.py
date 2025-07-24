d7782d3689a86332f55e5442e2cf723cb1db0c1e
Abhay Kumar Saini <abhaykumarsaini9982@gmail.com>
2025-07-24 10:24:15 +0530

    feat: Final test with new message

--- main.py
+++ main.py
@@ -4,4 +4,9 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "The full CI/CD pipeline works!"}
+    return {
+    "line1": "This is a new test of the complete pipeline.",
+    "line2": "Code pushed to GitHub triggers the build.",
+    "line3": "The deployment is running on Amazon EKS.",
+    "line4": "Success!"
+}
